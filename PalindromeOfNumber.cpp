2c0fc1c797e6edf62aad2c796f5f60d7aa476ae7
Anit <anit7795@gmail.com>
2026-05-21 14:52:17 +0530

    Palindrome of a number

--- PalindromeOfNumber.cpp
+++ PalindromeOfNumber.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int main() {
     int n;
-    int dup;
     int reverseNum =0;
     int lastDigit;
     cout << "Enter a number for palindrome: ";
     cin >> n;
+    int dup = n;
 
     
     while(n >0) {
         lastDigit = n %10;
-        
         n = n/10;
-
         reverseNum = (reverseNum *10) + lastDigit;
-        if(reverseNum == dup) {
-            cout << 
-        }
     }
 
+        if(dup == reverseNum) {
+            cout << "It is Palindrome";
+        } else {
+            cout << "The number is not a palindrome";
+        }
     return 0;
 }
