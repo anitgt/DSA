b83f7ff1834629f2b2979b617c8623ed4227e142
Anit <anit7795@gmail.com>
2026-05-21 14:45:26 +0530

    Reverse number

--- ReverseANumber.cpp
+++ ReverseANumber.cpp
@@ -12,7 +12,7 @@ int main() {
     while(n > 0) {
     LastDigit = n %10; // takes out last digit from original number
     n = n/10; //Eliminates last digit from original number
-    reverseNum = (reverseNum * 10) + LastDigit; 
+    reverseNum = (reverseNum * 10) + LastDigit; // reverses number by adding zero at the end and adding last digit of number
     }
 
     cout << reverseNum;
