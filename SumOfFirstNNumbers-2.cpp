6f509e7b593facc8180c758fca581fb1ff9e3d8a
Anit <anit7795@gmail.com>
2026-05-25 18:34:05 +0530

    Sum of first n numbers using recursion - function method

---
+++ SumOfFirstNNumbers-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int print(int n) {
+    if(n ==0) {
+        return 0;
+    }else {
+        return n + print(n -1);
+    }
+}
+
+int main() {
+   int n;
+   cin >> n;
+   
+   cout << print(n);
+    return 0;
+}
