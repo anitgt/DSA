852bf70ed311332d114c3f145bb5c1b92b8b2c7b
Anit <anit7795@gmail.com>
2026-05-26 00:47:41 +0530

    Reverse array using recursion

---
+++ ReverseAnArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void ReverseArray(int i, int arr[], int n) {
+    if(i > n/2) return;
+    else {
+        swap(arr[i], arr[n-i-1]);
+        ReverseArray(i+1, arr, n);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int arr[n];
+
+    for(int i=0; i <n; i++) {
+        cin >> arr[i];
+    };
+
+    ReverseArray(0,arr,n);
+
+    for(int i=0; i <n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+
+}
