76851a5316172c8af329a1441f119349b92fec9b
Anit <anit7795@gmail.com>
2026-05-30 12:29:04 +0530

    Comments for hasing of characters

--- CharacterHashing.cpp
+++ CharacterHashing.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-    string s;
+    string s; // takes string characters of alphabet from user.
     cin >> s;
 
     //precompute
     int hash[26] = {0};
     for(int i =0; i< s.size(); i++) {
-        hash[s[i] - 'a'] ++;
+        hash[s[i] - 'a'] ++; // here s[i] - 'a' is index od the character.
     }
-    int q;
+    int q; //takes number from user
     cin >> q;
     while(q--) {
         char c;
         cin >> c;
             //fetch
-            cout << hash[c - 'a'] << endl;;
+            cout << hash[c - 'a'] << endl; //c-'a' is index of the character.
     }
     return 0;
 }
