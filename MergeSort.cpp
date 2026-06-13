b4c607473c008a319511ba02937aeb4d0caa383f
Anit <anit7795@gmail.com>
2026-06-13 17:47:21 +0530

    Merge Sort

---
+++ MergeSort.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr,int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid +1;
+
+    while(left<=mid && right<=high) {
+        if(arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    };
+
+    while(left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    };
+
+    while(right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i=low; i<= high; i++) {
+        arr[i] = temp[i-low];
+    }
+
+}
+
+void merge_S(vector<int> &arr, int low, int high) {
+    int mid = (low + high) /2;
+    if(low >= high) return;
+    merge_S(arr, low, mid);
+    merge_S(arr, mid +1, high);
+    merge(arr, low, mid, high);
+};
+
+void merge_sort(vector<int> &arr, int n) {
+    merge_S(arr, 0, n-1);
+}
+
+
+int main() {
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i=0; i <n; i++) {
+        cout << "Enter value of " << i << ": ";
+        cin >> arr[i];
+    }
+
+    merge_sort(arr, n);
+
+    for(int i=0; i < n; i++) {
+        cout << "value of " << i << ": " <<  arr[i] << endl ;
+    }
+    return 0;
+}
