7c918f7bc36b81b90621f7f69477aa47204f85a4
Anit <anit7795@gmail.com>
2026-06-29 16:11:57 +0530

    Move all zeroes to the end of array optimal solution

--- MoveAllZeroesToTheEndOfTheArray.cpp
+++ MoveAllZeroesToTheEndOfTheArray.cpp
@@ -1,3 +1,5 @@
+/*
+
 //Brute Force Method
 #include<bits/stdc++.h>
 using namespace std;
@@ -29,6 +31,42 @@ int main() {
 
     for(int i=nonZero; i<n ; i++) {
         arr[i] = 0;
+    }
+
+    for(int i=0; i< n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+*/
+
+//Optimal Solution:
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+    int arr[n];
+    for(int i=0; i <n; i++) {
+        cin >> arr[i];
+    }
+
+    int j=-1;
+    for(int i=0; i<n; i++) {
+        if(arr[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+    for(int i=j+1; i < n; i++) {
+        if(arr[i] != 0) {
+            swap(arr[i], arr[j]);
+            j++;
+        }
     }
 
     for(int i=0; i< n; i++) {
