7669084700914410204847f5ce7ceea735fa4c3d
Anit <anit7795@gmail.com>
2026-06-30 17:44:21 +0530

    Intersection of two sorted arrays optimal solution

--- IntersectionOfTwoSortedArrays.cpp
+++ IntersectionOfTwoSortedArrays.cpp
@@ -1,3 +1,4 @@
+/*
 //Brute Force Method
 #include <bits/stdc++.h>
 using namespace std;
@@ -42,6 +43,58 @@ int main() {
 
     //print ans
     vector <int> ans = intersection(arr1, arr2);
+    for(int x : ans) {
+         cout << x << " ";
+     }
+
+    return 0;
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <int> intersection (vector <int> &a, vector <int> &b, int n, int m) {
+ int i=0;
+ int j =0;
+ vector <int> res;
+ 
+ while(i<n &&  j<m) {
+    if(a[i] < b[j]) {
+        i++;
+    }
+    else if(b[j] < a[i]) {
+        j++;
+    } else {
+        res.push_back(a[i]);
+        i++;
+        j++;
+    }
+}
+    return res;
+}
+
+
+int main() {
+      int n;
+    cout << "Enter N: ";
+    cin >> n;
+
+    int m;
+    cout << "Enter M: ";
+    cin >> m;
+    vector <int> arr1(n);
+    vector <int> arr2(m);
+
+    for(int i=0; i<n; i++) {
+        cin >> arr1[i];
+    };
+
+    for(int i=0; i<m; i++) {
+        cin >> arr2[i];
+    };
+
+    //print ans
+    vector <int> ans = intersection(arr1, arr2, n, m);
     for(int x : ans) {
          cout << x << " ";
      }
