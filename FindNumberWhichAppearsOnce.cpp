430266ca5a6d6698f9f646569325f408f94dd419
Anit <anit7795@gmail.com>
2026-07-12 19:54:44 +0530

    Find number which appeared once better solution using hashing

--- FindNumberWhichAppearsOnce.cpp
+++ FindNumberWhichAppearsOnce.cpp
@@ -1,4 +1,5 @@
 //Brute Force Approach
+/*
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -28,5 +29,46 @@ int main() {
     };
 
     cout << Number_Appeared_once(arr, n);
+    return 0;
+}
+*/
+
+//Better solution using hashing
+#include<bits/stdc++.h>
+using namespace std;
+
+int Number_Appeared_once(int arr[], int n) {
+    int maxi =arr[0];
+    for(int i=0; i<n; i++) {
+        maxi = max(maxi, arr[i]);
+    }
+
+    int hash[maxi+1] = {0};
+
+    //increase value of hash by 1 at that index
+    for(int i=0; i<n; i++) {
+        hash[arr[i]]++;
+    }
+
+    for(int i=0; i<n; i++) {
+        if(hash[arr[i]] == 1) {
+            return arr[i];
+        }
+    }
+}
+
+int main() {
+          int n;
+    cin >> n;
+
+    int arr[n];
+    
+    for(int i=0; i<n-1; i++) {
+        cin >> arr[i];
+    };
+
+    cout << Number_Appeared_once(arr, n);
+    return 0;
+
     return 0;
 }
