c5f3da93ec1ca296b098df926747bcf378f2dee1
Anit <anit7795@gmail.com>
2026-07-12 15:48:38 +0530

    Find Miissing number in an array better method with using hashing

--- FindMissingNumberInAnArray.cpp
+++ FindMissingNumberInAnArray.cpp
@@ -1,3 +1,4 @@
+/*
 //Brute Force Method
 #include<bits/stdc++.h>
 using namespace std;
@@ -32,3 +33,41 @@ int main() {
 
     return 0;
 }
+*/
+
+//Better Solution By using hashing
+#include<bits/stdc++.h>
+using namespace std;
+
+int missing_Num(int arr[], int n) {
+    int hash[n+1] =  {0};
+
+    for(int i=0; i<n-1; i++) {
+       hash[arr[i]] =1;
+    };
+
+    for(int i=1; i<n ; i++) {
+        if(hash[i] ==  0) {
+            return i;
+        };
+    };
+    return -1;
+};
+
+int main() {
+   int n;
+    cin >> n;
+
+    int arr[n-1];
+    
+    for(int i=0; i<n-1; i++) {
+        cin >> arr[i];
+    };
+
+    cout << missing_Num(arr, n);
+
+    return 0;
+}
+
+
+
